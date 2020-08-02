f877b4fbd896f45901303192dcd8707f1b6d15b4
Stephen Vincent <stevevincent2013@gmail.com>
2020-08-03 00:27:03 +0530

    Added Font Awesome CDN to base.html, Added navbar

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 def homepage_view(request):
     context = {
         "page_title": 'Home Page',
+        "home_isActive": 'active',
+        "about_isActive": '',
     }
 
     return render(request, '../templates/base.html', context)
