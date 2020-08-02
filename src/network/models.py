cae9c24bae4f4364a4471f304304a7b03a145f94
Stephen Vincent <stevevincent2013@gmail.com>
2020-08-02 14:25:44 +0530

    Added station,line, track models and migrated them

--- src/network/models.py
+++ src/network/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Station(models.Model):
+    station_name    = models.CharField(max_length=100, blank=False)
+
+class Line(models.Model):
+    line_color      = models.CharField(max_length=50, blank=False)
+    line_color_code = models.CharField(max_length=10, blank=False)
+
+class Track(models.Model):
+    source          = models.ForeignKey('Station', on_delete=models.CASCADE, related_name='tracks_to', blank=False)
+    destination     = models.ForeignKey('Station', on_delete=models.CASCADE, related_name='tracks_from', blank=False)
+    track_line      = models.ForeignKey('Line', on_delete=models.CASCADE, blank=False)
+    average_time    = models.PositiveIntegerField()
+    average_fare    = models.DecimalField(max_digits=8, decimal_places=2)
