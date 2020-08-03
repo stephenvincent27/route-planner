ccc3deb83464599e106bc6962e03a51e19912972
Stephen Vincent <stevevincent2013@gmail.com>
2020-08-04 01:04:39 +0530

    Modified the About page

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,7 +4,12 @@ def homepage_view(request):
     context = {
         "page_title": 'Find Route',
         "home_isActive": 'active',
+        "listOfStations": '',
         "about_isActive": '',
     }
 
     return render(request, '../templates/base.html', context)
+
+def about_view(request):
+    context = {}
+    return render(request, '../templates/about.html', context)
--- src/route_planner/urls.py
+++ src/route_planner/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import homepage_view 
+from pages.views import homepage_view , about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', homepage_view)
+    path('', homepage_view),
+    path('about/', about_view),
 ]
