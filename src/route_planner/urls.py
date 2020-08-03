1d098a1e1d8ee5cf62beb6ac821be23e93795ea2
Stephen Vincent <stevevincent2013@gmail.com>
2020-08-04 01:17:52 +0530

    Added CSS file

--- src/route_planner/urls.py
+++ src/route_planner/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import homepage_view , about_view
+from pages.views import homepage_view, about_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
