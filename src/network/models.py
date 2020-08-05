32de76ad4711006125132f224704e60728d66af9
Stephen Vincent <stevevincent2013@gmail.com>
2020-08-05 16:12:03 +0530

    Added forms.py, added __str__ function to models

--- src/network/models.py
+++ src/network/models.py
@@ -3,9 +3,15 @@ from django.db import models
 class Station(models.Model):
     station_name    = models.CharField(max_length=100, blank=False)
 
+    def __str__(self):
+        return self.station_name
+
 class Line(models.Model):
     line_color      = models.CharField(max_length=50, blank=False)
     line_color_code = models.CharField(max_length=10, blank=False)
+
+    def __str__(self):
+        return self.line_color
 
 class Track(models.Model):
     source          = models.ForeignKey('Station', on_delete=models.CASCADE, related_name='tracks_to', blank=False)
