f4db4be057cc1b3da681a6ae3bfc1c9c4c3ce3ab
Stephen Vincent <stevevincent2013@gmail.com>
2022-01-05 20:57:42 +0530

    Add site URL to list of allowed hosts

--- src/route_planner/settings.py
+++ src/route_planner/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'routeplanner.pythonanywhere.com',
+]
 
 
 # Application definition
